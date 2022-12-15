e73bd1f5d4d9ae1636fc1d7e1508b87f8d8a9f1e
Ricardo Pinto <ricardoschillermail@gmail.com>
2022-12-15 15:45:05 +0000

    Added web app

---
+++ Agap2IT Academy SuperMarket ConsoleApp/Agap2IT.Academy.SuperMarket.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Agap2IT.Academy.SuperMarket.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
